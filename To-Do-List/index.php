f6131e054b2d5dc4229cd88028483b75c45ee8e4
Ivan Harvey Danao Rivera <113413611+Xplizit-NM7@users.noreply.github.com>
2026-04-22 22:26:16 +0800

    index.php updates to do list

--- To-Do-List/index.php
+++ To-Do-List/index.php
@@ -1,22 +1,44 @@
-<div id="listView">
+<div id="tasks">
     
 </div>
 
 <script>
-    const tasks = document.getElementById("tasks");
+    
+    const taskList = document.getElementById("tasks");
+    const taskInput = document.querySelector("#taskInput");
+    const addBtn = document.querySelector("addBtn");
 
+
+
+    //Load Tasks
     async function loadTasks() {
-    const response = await fetch("ctrl_index.php",{
-        method: "POST",
-        body:"tasks"
-    });
+        const response = await fetch("ctrl_index.php");
 
-    if (response.ok){
-        const data = await response.json();
-        return data.result;
-    }else{
-        thro new Error(`Server error: ${response.status}`);
+        if (response.ok){
+            const data = await response.json();
+            
+            data.forEach(task => {
+                const div = document.createElement("div");
+                div.textContent = task.listContent;
+                taskList.appendChild(div);
+            });
+
+        } else{
+            throw new Error(`Server error: ${response.status}`);
+        }
     }
-        
+
+    loadTasks();
+
+    //Create
+    async function loadTasks(){
+        const response = await fetch("ctrl_index.php",{
+        method: "POST",
+        body: formData
+        });
+
+
     }
+
+
 </script>
